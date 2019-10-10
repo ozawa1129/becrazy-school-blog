1d73f8f6ca3ee95cf251e638acaa529cde6184b1
小澤玄 <>
2019-10-10 11:34:20 +0900

    論理削除の機能を追加

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Blog extends Model
 {
-    //
+    //論理削除機能を追加
+    use SoftDeletes;
+    // カラム名の設定上書き
+    const DELETED_AT = 'deleted_datetime';
 }
