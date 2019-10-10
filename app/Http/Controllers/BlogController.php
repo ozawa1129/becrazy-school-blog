18120e0835522081e4fb197c3f692c420635ee6c
小澤玄 <>
2019-10-10 11:42:48 +0900

    使用モデルをBlogに変更

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\Blog;
 
 class BlogController extends Controller {
     
