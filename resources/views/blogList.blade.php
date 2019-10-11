85aa52c2b29b1b90a06bf4e83785f68cbec9c2ff
小澤玄 <>
2019-10-11 14:39:12 +0900

    記事一覧を表示させるbladeファイル生成

---
+++ resources/views/blogList.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="検索キーワード">
+    <meta name="description" content="説明文">
+    <title>記事一覧</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/library.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+    <header>
+        <h1>ブログの記事一覧</h1>
+    </header>
+    <article id="sample">ここにいろんな文を書いたり画像を置いたり…</article>
+    <footer>
+    </footer>
+</body>
+</html>
