793d6bd798ba379ad4db9e4b472b4a614f868ce3
小澤玄 <>
2019-10-11 17:40:53 +0900

    Postするデータを増やした。あとでinputタグを細かく修正

--- resources/views/blogAddForm.blade.php
+++ resources/views/blogAddForm.blade.php
@@ -14,7 +14,28 @@
     <header>
         <h1>記事を追加する</h1>
     </header>
-    <article id="sample">ここにいろんな文を書いたり画像を置いたり…</article>
+    <div class="add">
+        <form method="POST" action="blogAdd">
+            @csrf
+            <!-- 一時的に1を送信 -->
+            <input type="hidden" name="user_id" value="1">
+            <!-- 一時的にpublishを送信 -->
+            <input type="hidden" name="status" value="publish">
+            <!-- 一時的にtestを送信 -->
+            <input type="hidden" name="slug" value="test">
+            <!-- 一時的にtext/htmlを送信 -->
+            <input type="hidden" name="mime_type" value="text/html">
+            <dl>
+                <dt>タイトル</dt>
+                <dd><input type="text" name="title" required value="{{ old('title') }}"></dd>
+            </dl>
+            <dl>
+                <dt>本文</dt>
+                <dd><textarea name="content" required>{{ old('content')}}</textarea></dd>
+            </dl>
+            <input type="submit" value="ブログ投稿">
+        </form>
+    </div>
     <footer>
     </footer>
 </body>
