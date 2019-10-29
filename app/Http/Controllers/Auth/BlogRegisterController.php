b0413fa11f3aba6a8a60aef50973248111314173
小澤玄 <>
2019-10-29 12:07:00 +0900

    user数に応じて飛ばすページを変えるメソッドを記述

--- app/Http/Controllers/Auth/BlogRegisterController.php
+++ app/Http/Controllers/Auth/BlogRegisterController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Controllers\Auth;
 
-class BlogRegisterController extends RegistersController
+use App\Models\User;
+
+class BlogRegisterController extends RegisterController
 {
     /**
      * Where to redirect users after registration.
@@ -10,4 +12,13 @@ class BlogRegisterController extends RegistersController
      */
     // ログイン後に遷移するurl指定
     protected $redirectTo = '/';
+    
+    public function checkRegister(){
+        $users = User::all()->count();
+        if($users === 0){
+            return redirect('register');
+        }else{
+            return redirect('blogTop');
+        }
+    }
 }
