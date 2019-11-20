16ce2a3bf0bd5024b3858c341de017cb013e9351
小澤玄 <>
2019-11-20 19:41:52 +0900

    レイアウトに合わせてbladeの変更

--- resources/views/categoryChoice.blade.php
+++ resources/views/categoryChoice.blade.php
@@ -2,14 +2,20 @@
 
 @section('title', 'カテゴリー選択')
 
-@section('h1', 'カテゴリー、タグ一覧から選択')
+
+<!-- @section('h1', 'カテゴリー、タグ一覧から選択') -->
 
 @section('content')
-    <div class="lists">
-        @foreach ($categories as $category)
-            <ul>
-                <li><a href="categoryGroup/{{$category->id}}">{{ $category->name }}</a></li>
-            </ul>
-        @endforeach
+    <div class="col-md-9 content">
+        <div class="row bg-light justify-content-center article">
+            <div class="category col-md-12">
+                @foreach ($categories as $category)
+                    <ul>
+                        <li><a href="/categoryGroup/{{$category->id}}">■{{ $category->name }}</a></li>
+                        <li>説明文:{{ $category->description }}</li>
+                    </ul>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
