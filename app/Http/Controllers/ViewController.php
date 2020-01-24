7f97898dda3b64562a2f13361631845cde67dbe5
小澤玄 <56157351+ozawa1129@users.noreply.github.com>
2020-01-24 21:15:00 +0900

    テスト用のルーティングとbladeを作成

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -14,24 +14,29 @@ class ViewController extends Controller {
     ブログ閲覧機能で使うメソッド
     -----------------------
     */
-    
+
+    // テスト用ページ
+    public function testpage(){
+        return view('test');
+    }
+
     // トップページ表示
     public function blogTop(){
         $posts = Post::where('status', 'publish')->orderBy('id', 'DESC')->take(5)->get();
         $data = array('latestFive' => $posts);
         return view('blogTop', $data);
     }
-    
+
     // 記事ページ
     public function blogArticle($slug){
         $posts = Post::where('slug', $slug)->first();
         if($posts->status === 'publish'){
             $prev = Post::where('id', '<', $posts->id)->orderBy('id', 'desc')->limit('1')->first();
             $next = Post::where('id', '>', $posts->id)->orderBy('id')->limit('1')->first();
-        
+
             // 全部取る
             $taxonomy = $posts->taxonomies;
-        
+
             // tagとcategoryに分ける
             $tags = array();
             $categories = array();
@@ -46,18 +51,18 @@ class ViewController extends Controller {
             $data = array('article' => $posts, 'prev' => $prev, 'next' => $next, 'tags' => $tags, 'categories' => $categories);
             return view('blogArticle', $data);
         }else{
-            
+
         }
 
     }
-    
+
     // カテゴリー選択時の記事一覧
     public function categoryChoice(){
         $categories = Taxonomy::whereIn('type', ["category"])->get();
         $data = array('categories' => $categories);
         return view('categoryChoice', $data);
     }
-    
+
     // カテゴリー選択時に飛ばす
     public function categoryGroup($id){
         $taxonomies = Taxonomy::find($id);
